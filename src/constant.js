20186aa557d617e5f76fec6dadf22bb045e623fd
zeeshan038 <hankhan123623@gmail.com>
2025-07-12 14:28:19 +0500

    first commit

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://new-alshifa-backend-2.onrender.com"
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "https://makkah-hospital-backend.onrender.com"
+// export const BASE_URL = "http://localhost:5000"
 
