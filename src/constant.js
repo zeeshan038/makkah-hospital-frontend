26983c63499c3e8b3b85260af47ca2998258118f
zeeshan038 <hankhan123623@gmail.com>
2025-07-21 08:14:00 +0500

    URL Changed

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://makkah-hospital-backend.onrender.com"
+export const BASE_URL = "https://makkah-hospital-backend-9t46.onrender.com"
 // export const BASE_URL = "http://localhost:5000"
 
